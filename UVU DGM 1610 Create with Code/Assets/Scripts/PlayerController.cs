0e2e0b7cb943a48fcc88991c9fd066a169835fab
Jake <jaketelos7@gmail.com>
2020-09-14 17:43:24 -0600

    Created Input script

---
+++ UVU DGM 1610 Create with Code/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+
+    public float turnSpeed;
+
+    public float hInput;
+
+    public float fInput;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * hInput);
+    }
+}
