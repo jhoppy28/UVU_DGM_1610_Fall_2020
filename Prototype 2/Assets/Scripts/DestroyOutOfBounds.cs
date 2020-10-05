700dd22e70419016bf8d190f0aa01bbeb099cdd3
Jake <jaketelos7@gmail.com>
2020-10-05 17:24:28 -0600

    Completed lesson 2.4

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -8,6 +8,11 @@ public class DestroyOutOfBounds : MonoBehaviour
     public float topBounds = 30f;
     public float lowerBounds = -10f; 
 
+    void Awake()
+    {
+        Time.timeScale = 1;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -20,7 +25,9 @@ public class DestroyOutOfBounds : MonoBehaviour
         //Destroys any object entering the lowerBounds
         else if (transform.position.z < lowerBounds)
         {
+            Debug.Log("GAME OVER");
             Destroy(gameObject);
+            Time.timeScale = 0;
         }
     }
 }
--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -8,18 +8,27 @@ public class SpawnManager : MonoBehaviour
 
     public int animalIndex;
     public float spawnRangeX = 20.5f;
-    public float spawnPosZ = 0f; 
+    public float spawnPosZ = 0f;
+    public float startDelay, spawnInterval; 
+
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimals", startDelay, spawnInterval);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            //Randomly generate animals at a random location
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Debug.Log(animalIndex);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+
+    }
+
+    void SpawnRandomAnimals()
+    {
+        //Randomly generate animals at a random location
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Debug.Log(animalIndex);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
