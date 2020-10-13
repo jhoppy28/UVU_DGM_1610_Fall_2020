8252ea2dc65d14704faba3d6a55edc112f6161ef
Jake <jaketelos7@gmail.com>
2020-10-12 18:46:05 -0600

    Created Prototype 3

---
+++ Prototype 3/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
