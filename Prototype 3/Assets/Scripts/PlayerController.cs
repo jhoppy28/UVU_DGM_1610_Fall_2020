a05ca9f338e549af7e384a17f42dd34d10cad50d
Jake <jaketelos7@gmail.com>
2020-10-21 17:00:14 -0600

    Completed jump and fall animations.

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -9,21 +9,25 @@ public class PlayerController : MonoBehaviour
     public float gravityMod;
     public bool isGrounded = true;
     public bool gameOver = false;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityMod;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true && !gameOver)
         {
             isGrounded = false;
             playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
+
         }
     }
 
@@ -39,6 +43,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver = true;
             Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
