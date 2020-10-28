0652304d79af2a827c9ec1cfb22f50d0c1aed1c2
Jake <jaketelos7@gmail.com>
2020-10-28 11:52:28 -0600

    Attempted to fix issues with prototype 3

--- Prototype 3/Assets/Scripts/SpawnManager.cs
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,23 +6,22 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-    private float startDelay;
-    private float repeatDelay;
+    private float startDelay = 2.0f;
+    private float repeatDelay = 2.0f;
     private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawn Obstacles", startDelay, repeatDelay);
+        InvokeRepeating("SpawnObstacles", startDelay, repeatDelay);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void spawnObstacles()
     {
-        if(playerControllerScript.gameOver = false)
+        if (playerControllerScript.gameOver == false)
         {
             Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
         }
-        
     }
 }
