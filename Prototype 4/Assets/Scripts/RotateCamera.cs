f86fefa3555fe07d4557b3b20001059689171fc6
Jake <jaketelos7@gmail.com>
2020-11-04 17:19:01 -0700

    Completed Lesson 4.1

---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    //Allows the camera rotation speed to be accessed in the inspector
+    public float rotationSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get horizontal input for camera rotation around the z-axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+}
